6eb2c4f31f3f418805c86637dd5f0d79ae201df0
Tolgetmen <Tolgetmen@gmail.com>
2023-11-19 01:13:21 +0300

    Task006Test change parent "SS" without xml

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -4,16 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val textLine = findViewById<TextView>(R.id.titleTextView)
-        val button = findViewById<Button>(R.id.changeButton)
+
+        val linearLayout = LinearLayout(this)
+        linearLayout.orientation = LinearLayout.VERTICAL
+        linearLayout.id = R.id.rootLayout
+
+        val textView = TextView(this)
+        textView.id = R.id.titleTextView
+        textView.text = "Hello World!"
+        linearLayout.addView(textView)
+
+        val button = Button(this)
+        button.id = R.id.changeButton
+        button.text="change"
+        linearLayout.addView(button)
+
+        setContentView(linearLayout)
+
+
         button.setOnClickListener{
-            textLine.text = "I am an Android Developer!"
+            textView.text = "I am an Android Developer!"
         }
 
     }
