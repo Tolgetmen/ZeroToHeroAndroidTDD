6a367f6fdd7f8a51ec7f21e7452522ad5a75bc85
Tolgetmen <Tolgetmen@gmail.com>
2023-11-19 23:48:36 +0300

    Task007Test change parent "DS" used code

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -8,18 +8,30 @@ import android.widget.LinearLayout
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView = findViewById<TextView>(R.id.titleTextView)
         val button = findViewById<Button>(R.id.changeButton)
-
+        textView = findViewById(R.id.titleTextView)
 
         button.setOnClickListener {
             textView.text = "I am an Android Developer!"
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("key", textView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textView.text = savedInstanceState.getString("key")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 
 
