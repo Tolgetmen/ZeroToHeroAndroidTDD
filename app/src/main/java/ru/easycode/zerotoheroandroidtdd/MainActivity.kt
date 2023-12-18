1b19f4038a5f4b6fefe134391a8f6bd2a038c99f
Tolgetmen <Tolgetmen@gmail.com>
2023-12-18 22:45:06 +0300

    Task014Test progress solved 1 ver "SD"

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,6 +1,10 @@
 package ru.easycode.zerotoheroandroidtdd
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
@@ -8,9 +12,37 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var textView: TextView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var button: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        textView = findViewById(R.id.titleTextView)
+        progressBar = findViewById(R.id.progressBar)
+        button = findViewById(R.id.actionButton)
+
+        button.setOnClickListener {
+            button.isEnabled = false
+            progressBar.visibility = View.VISIBLE
+
+            button.postDelayed({
+                textView.visibility = View.VISIBLE
+                progressBar.visibility = View.INVISIBLE
+                button.isEnabled = true
+            },3000)
+
+
+
+
+        }
+
+
     }
+
+
 }
