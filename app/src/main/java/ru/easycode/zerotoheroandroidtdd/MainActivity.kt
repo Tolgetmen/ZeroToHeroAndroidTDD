9579d2b43014d58543caae526c111ca646057596
Tolgetmen <Tolgetmen@gmail.com>
2024-02-15 01:18:44 +0300

    Task021Test watch-input solved "SD" 1

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,41 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val textWatcher = object : SimpleTextWatcher() {
+
+            override fun afterTextChanged(s: Editable?) {
+                binding.actionButton.isEnabled = binding.inputEditText.text.toString().length >= 3
+//            binding.actionButton.isEnabled = binding.inputEditText.text!!.length >= 3
+            }
+        }
+
+
+        binding.actionButton.setOnClickListener {
+            binding.titleTextView.text = binding.inputEditText.text
+            binding.inputEditText.removeTextChangedListener(textWatcher)
+            binding.inputEditText.setText("")
+            binding.actionButton.isEnabled = false
+            binding.inputEditText.addTextChangedListener(textWatcher)
+        }
+
+        binding.inputEditText.addTextChangedListener(textWatcher)
     }
+}
+
+private abstract class SimpleTextWatcher : TextWatcher {
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+    override fun afterTextChanged(s: Editable?) = Unit
 }
